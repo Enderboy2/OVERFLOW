9e7e1558e9f5405611d095892189519151b30297
Enderboy2 <y01006452923@gmail.com>
2025-04-06 17:21:22 +0200

    you

---
+++ atlas/taskig/animal.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self):
+        self.no_legs = 4
+        self.no_eyes = 2
+        self.animal_sound = ""
+        self.owner_name = ""
+    def talk(self):
+        print(self.animal_sound)
+
+def adddddd(a,b):
+    return a + b
